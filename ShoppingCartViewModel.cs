9d2978092af98fa5aa55808eb58104bb73e3872b
Sachin Singh <37268132+AskSachinSingh@users.noreply.github.com>
2018-04-05 16:11:15 +0530

    Add files via upload

---
+++ ShoppingCartViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using MusicWorld.Models;
+
+namespace MusicWorld.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
